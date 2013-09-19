eb135799849dc42de5a8c8ebd5c042a67a6ebba6
Diego Medici <diego.medici@studiofarma.it>
2013-09-19 15:53:33 +0200

    Primo commit per la solution

---
+++ Prova/Prova/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prova per GIT");
+            Console.ReadLine();
+        }
+    }
+}
